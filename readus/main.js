6a82641eb21a5271bbc99cadfc8a606bd804d2f2
ighodarodaniel1995 <ighodarod95@gmail.com>
2022-05-14 23:09:20 +0200

    a simple website

---
+++ readus/main.js
@@ -0,0 +1,25 @@
+const companies = [
+    {name:"company one",category:"finance",start:1908, end:2008},
+    {name:"company two",category:"finance",start:1908, end:2008},
+    {name:"company three",category:"finance",start:1908, end:2008},
+
+{name:"company four",category:"finance",start:1908, end:2008},
+{name:"company five",category:"finance",start:1908, end:2008},
+{name:"company six",category:"finance",start:1908, end:2008},
+{name:"company seven",category:"finance",start:1908, end:2008},
+{name:"company eight",category:"",start:1908, end:2008}] ;
+
+const age = [21,25,40,35,15,89,72,58,90,12,22,23];
+
+
+
+companies.forEach(company => console.log(company.start));
+
+const digit = [9,8,7,6,5,4,3,2];
+console.log(digit.indexOf(3));
+
+
+let add = 8 + null ;
+console.log(add);
+let sub = null == 0
+console.log(sub);
